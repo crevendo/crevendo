f8aad22e312aedd6d9c591f0db3d5285505d3062
jarlisteo <jarlisteo@gmail.com>
2023-03-23 11:48:13 -0600

    version update

--- hook/order.pb.validate.go
+++ hook/order.pb.validate.go
@@ -35,6 +35,142 @@ var (
 	_ = sort.Sort
 )
 
+// Validate checks the field values on OrderStatusesHookParams with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *OrderStatusesHookParams) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on OrderStatusesHookParams with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// OrderStatusesHookParamsMultiError, or nil if none found.
+func (m *OrderStatusesHookParams) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *OrderStatusesHookParams) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	for idx, item := range m.GetOrderStatuses() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, OrderStatusesHookParamsValidationError{
+						field:  fmt.Sprintf("OrderStatuses[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, OrderStatusesHookParamsValidationError{
+						field:  fmt.Sprintf("OrderStatuses[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return OrderStatusesHookParamsValidationError{
+					field:  fmt.Sprintf("OrderStatuses[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return OrderStatusesHookParamsMultiError(errors)
+	}
+
+	return nil
+}
+
+// OrderStatusesHookParamsMultiError is an error wrapping multiple validation
+// errors returned by OrderStatusesHookParams.ValidateAll() if the designated
+// constraints aren't met.
+type OrderStatusesHookParamsMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m OrderStatusesHookParamsMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m OrderStatusesHookParamsMultiError) AllErrors() []error { return m }
+
+// OrderStatusesHookParamsValidationError is the validation error returned by
+// OrderStatusesHookParams.Validate if the designated constraints aren't met.
+type OrderStatusesHookParamsValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e OrderStatusesHookParamsValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e OrderStatusesHookParamsValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e OrderStatusesHookParamsValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e OrderStatusesHookParamsValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e OrderStatusesHookParamsValidationError) ErrorName() string {
+	return "OrderStatusesHookParamsValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e OrderStatusesHookParamsValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sOrderStatusesHookParams.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = OrderStatusesHookParamsValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = OrderStatusesHookParamsValidationError{}
+
 // Validate checks the field values on OrderCreateHookParams with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
--- hook/order_grpc.pb.go
+++ hook/order_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type OrderHooksClient interface {
 	OrderCreated(ctx context.Context, in *OrderCreateHookParams, opts ...grpc.CallOption) (*OrderCreateHookParams, error)
+	OrderStatuses(ctx context.Context, in *OrderStatusesHookParams, opts ...grpc.CallOption) (*OrderStatusesHookParams, error)
 }
 
 type orderHooksClient struct {
@@ -42,11 +43,21 @@ func (c *orderHooksClient) OrderCreated(ctx context.Context, in *OrderCreateHook
 	return out, nil
 }
 
+func (c *orderHooksClient) OrderStatuses(ctx context.Context, in *OrderStatusesHookParams, opts ...grpc.CallOption) (*OrderStatusesHookParams, error) {
+	out := new(OrderStatusesHookParams)
+	err := c.cc.Invoke(ctx, "/OrderHooks/OrderStatuses", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // OrderHooksServer is the server API for OrderHooks service.
 // All implementations must embed UnimplementedOrderHooksServer
 // for forward compatibility
 type OrderHooksServer interface {
 	OrderCreated(context.Context, *OrderCreateHookParams) (*OrderCreateHookParams, error)
+	OrderStatuses(context.Context, *OrderStatusesHookParams) (*OrderStatusesHookParams, error)
 	mustEmbedUnimplementedOrderHooksServer()
 }
 
@@ -56,6 +67,9 @@ type UnimplementedOrderHooksServer struct {
 
 func (UnimplementedOrderHooksServer) OrderCreated(context.Context, *OrderCreateHookParams) (*OrderCreateHookParams, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method OrderCreated not implemented")
+}
+func (UnimplementedOrderHooksServer) OrderStatuses(context.Context, *OrderStatusesHookParams) (*OrderStatusesHookParams, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method OrderStatuses not implemented")
 }
 func (UnimplementedOrderHooksServer) mustEmbedUnimplementedOrderHooksServer() {}
 
@@ -88,6 +102,24 @@ func _OrderHooks_OrderCreated_Handler(srv interface{}, ctx context.Context, dec 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _OrderHooks_OrderStatuses_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(OrderStatusesHookParams)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(OrderHooksServer).OrderStatuses(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/OrderHooks/OrderStatuses",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(OrderHooksServer).OrderStatuses(ctx, req.(*OrderStatusesHookParams))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // OrderHooks_ServiceDesc is the grpc.ServiceDesc for OrderHooks service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +130,10 @@ var OrderHooks_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "OrderCreated",
 			Handler:    _OrderHooks_OrderCreated_Handler,
+		},
+		{
+			MethodName: "OrderStatuses",
+			Handler:    _OrderHooks_OrderStatuses_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
