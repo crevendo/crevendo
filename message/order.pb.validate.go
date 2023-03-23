6c83c213f219feede6ec8c55cd6d7c67d48ebfc2
jarlisteo <jarlisteo@gmail.com>
2023-03-23 10:07:45 -0600

    version update

--- message/order.pb.validate.go
+++ message/order.pb.validate.go
@@ -1097,6 +1097,40 @@ func (m *OrderStatusListResponse) validate(all bool) error {
 
 	var errors []error
 
+	for idx, item := range m.GetOrderStatuses() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, OrderStatusListResponseValidationError{
+						field:  fmt.Sprintf("OrderStatuses[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, OrderStatusListResponseValidationError{
+						field:  fmt.Sprintf("OrderStatuses[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return OrderStatusListResponseValidationError{
+					field:  fmt.Sprintf("OrderStatuses[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return OrderStatusListResponseMultiError(errors)
 	}
