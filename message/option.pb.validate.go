93ffaeb95a5ded5f4fd1b7be304238b1ac9e1741
jarlisteo <jarlisteo@gmail.com>
2023-03-27 11:47:39 -0600

    version update

--- message/option.pb.validate.go
+++ message/option.pb.validate.go
@@ -295,38 +295,33 @@ func (m *OptionUpdateMessage) validate(all bool) error {
 
 	var errors []error
 
-	for idx, item := range m.GetOptions() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, OptionUpdateMessageValidationError{
-						field:  fmt.Sprintf("Options[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, OptionUpdateMessageValidationError{
-						field:  fmt.Sprintf("Options[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return OptionUpdateMessageValidationError{
-					field:  fmt.Sprintf("Options[%v]", idx),
+	if all {
+		switch v := interface{}(m.GetOption()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, OptionUpdateMessageValidationError{
+					field:  "Option",
 					reason: "embedded message failed validation",
 					cause:  err,
-				}
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, OptionUpdateMessageValidationError{
+					field:  "Option",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
 			}
 		}
-
+	} else if v, ok := interface{}(m.GetOption()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return OptionUpdateMessageValidationError{
+				field:  "Option",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
 	}
 
 	if len(errors) > 0 {
