99408983101f697ccdbb33cac7500fc0cf73a61c
jarlisteo <jarlisteo@gmail.com>
2023-04-21 13:42:32 -0600

    version update

--- message/address.pb.validate.go
+++ message/address.pb.validate.go
@@ -1000,6 +1000,40 @@ func (m *AddressUpdateMessage) validate(all bool) error {
 
 	// no validation rules for Notes
 
+	for idx, item := range m.GetFields() {
+		_, _ = idx, item
+
+		if all {
+			switch v := interface{}(item).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, AddressUpdateMessageValidationError{
+						field:  fmt.Sprintf("Fields[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, AddressUpdateMessageValidationError{
+						field:  fmt.Sprintf("Fields[%v]", idx),
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return AddressUpdateMessageValidationError{
+					field:  fmt.Sprintf("Fields[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
 	if len(errors) > 0 {
 		return AddressUpdateMessageMultiError(errors)
 	}
